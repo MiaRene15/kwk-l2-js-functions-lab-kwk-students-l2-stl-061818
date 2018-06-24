8c10bf859d7235a66dafce52fc275d840e92eca2
MiaRene15 <kids.hines@icloud.com>
2018-06-24 21:08:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function distanceFromHqInBlocks(block) {
 function distanceFromHqInFeet(block) {
   distanceFromHqInBlocks(block);
   return calc 
-  
   let feetCalc = block*264
   return feetCalc
   
