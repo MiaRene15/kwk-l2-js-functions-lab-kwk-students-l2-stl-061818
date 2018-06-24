d23a7c2c0fc6bf9d682071cc9f62e324dd1a0a8c
MiaRene15 <kids.hines@icloud.com>
2018-06-24 20:23:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+// function distanceFromHqInBlocks(someValue) {
+//      let someValue = 268
+//}
+
+//function distanceTravelledInFeet(someValue, someValue2) // {
+ //let math = someValue2 - someValue * 264
+ //return math
+//}
+
+//function distanceFromHqInBlocks(43) {
+//  let math = someValue * 264 
+//  return math 
+//}
