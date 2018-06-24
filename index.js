513c898afeb51c39057c40e6bb64d4e980ed2cd4
MiaRene15 <kids.hines@icloud.com>
2018-06-24 21:32:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,6 @@ function distanceFromHqInFeet(block) {
 }
 
 function distanceTravelledInFeet(block1, block2) {
-  if (block1 > block2) {
-  let feettravel = (block2-block1)*264
-  return feettravel
-  } else if (block2 < block1) {
-  let feettravel = (block1-block2)*264
-  return feettravel
-  }
+  let feetTravel = (block2 - block1) * 264
+  return feetTravel
 }
