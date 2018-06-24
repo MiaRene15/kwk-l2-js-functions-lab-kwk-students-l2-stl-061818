cddb0c821be2e32ca7e3d52486d15e1cf285b666
MiaRene15 <kids.hines@icloud.com>
2018-06-24 20:45:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks(block) {
- let calc = block - 42 
- return calc
-}
+//function distanceFromHqInBlocks(block) {
+// let calc = block - 42 
+// return calc
+// }
