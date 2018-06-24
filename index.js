5699439bd0d93ea4d0800fc09ecb154aec120290
MiaRene15 <kids.hines@icloud.com>
2018-06-24 20:43:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 // Code your solution in this file!
 
 function distanceFromHqInBlocks(block) {
- if (block < 42) {
  let calc = block - 42 
  return calc
- } else (block >= 42) {
-   let calc = 42 - block 
-   return calc
- }
  
 }
